716bd6409fe1285da993f2e66d3c0eb2ad478548
sgbalogh <sgbalogh@gmail.com>
2015-05-05 12:45:41 -0400

    progress made on faceting feature; simple subtractive faceting now working with text fields

--- exhibits/themes/citations/show.php
+++ exhibits/themes/citations/show.php
@@ -17,8 +17,28 @@
 
 <!-- Zoom buttons. -->
 <div id="zoom">
-  <div class="testtags">test tags</div>
-  <div class="btn out">action</div>
+      <div class="AdditionalCareers">AdditionalCareers</div>
+  	  <div class="AntiSemiticRhetoric">AntiSemiticRhetoric</div>
+  	  <div class="Bartering">Bartering</div>
+  	  <div class="BusinessPractices">BusinessPractices</div>
+  	  <div class="ConflictViolenceCommunal">ConflictViolenceCommunal</div>
+  	  <div class="ConflictViolencePersonal">ConflictViolencePersonal</div>
+  	  <div class="ContextualResource">ContextualResource</div>
+  	  <div class="CulturalExchange">CulturalExchange</div>
+  	  <div class="Family">Family</div>
+  	  <div class="LiteraryArtisticDepiction">LiteraryArtisticDepiction</div>
+  	  <div class="NonPeddlingBackground">NonPeddlingBackground</div>
+  	  <div class="PeddlingBackground">PeddlingBackground</div>
+  	  <div class="ReligiousPractices">ReligiousPractices</div>
+  	  <div class="RestrictionsCommerce">RestrictionsCommerce</div>
+  	  <div class="Routes">Routes</div>
+  	  <div class="Sponsor">Sponsor</div>
+  	  <div class="SubsequentCommercialCareers">SubsequentCommercialCareers</div>
+  	  <div class="SubsequentNonCommercial">SubsequentNonCommercial</div>
+  	  <div class="Transportation">Transportation</div>
+  	  <div class="VictimsOfCrime">VictimsOfCrime</div>
+  	  <div class="Wares">Wares</div>
+	<div class="reset">RESET</div>
 </div>
  
 <?php echo foot(); ?>
